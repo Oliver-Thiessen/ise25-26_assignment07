bfef95cb30a903cbe359ca6aa96df48cd9d199c0
Oliver Thiessen <oliver.thiessen@stud.uni-heidelberg.de>
2025-12-09 11:08:15 +0100

    Endpunkte und Klasse ReviewDto ohne Boolean approved definiert

--- api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/controller/ReviewController.java
@@ -1,12 +1,15 @@
 package de.seuhd.campuscoffee.api.controller;
 
 import de.seuhd.campuscoffee.api.dtos.ReviewDto;
+import de.seuhd.campuscoffee.api.dtos.UserDto;
 import de.seuhd.campuscoffee.api.mapper.DtoMapper;
 import de.seuhd.campuscoffee.api.openapi.CrudOperation;
 import de.seuhd.campuscoffee.domain.model.objects.Review;
 import de.seuhd.campuscoffee.domain.ports.api.CrudService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
@@ -18,6 +21,7 @@ import java.util.List;
 
 import static de.seuhd.campuscoffee.api.openapi.Operation.*;
 import static de.seuhd.campuscoffee.api.openapi.Resource.REVIEW;
+import static de.seuhd.campuscoffee.api.openapi.Resource.USER;
 
 /**
  * Controller for handling reviews for POS, authored by users.
@@ -48,5 +52,70 @@ public class ReviewController extends CrudController<Review, ReviewDto, Long> {
         return super.getAll();
     }
 
-    // TODO: Implement the missing methods/endpoints.
+    // TODO: x Implement the missing methods/endpoints.
+
+    @Operation
+    @CrudOperation(operation=GET_BY_ID, resource=REVIEW)
+    @GetMapping("/{id}")
+    public @NonNull ResponseEntity<ReviewDto> getById(
+        @Parameter(description="Unique identifier of the review to retrieve.", required=true)
+        @PathVariable Long id) {
+        return super.getById(id);
+    }
+
+    @Operation
+    @CrudOperation(operation=CREATE, resource=REVIEW)
+    @PostMapping("")
+    public @NonNull ResponseEntity<ReviewDto> create(
+        @Parameter(description="Data of the review to create.", required=true)
+        @RequestBody @Valid ReviewDto reviewDto) {
+        return super.create(reviewDto);
+    }
+
+    @Operation
+    @CrudOperation(operation=UPDATE, resource=REVIEW)
+    @PutMapping("/{id}")
+    public @NonNull ResponseEntity<ReviewDto> update(
+        @Parameter(description="Unique identifier of the review to update.", required=true)
+        @PathVariable Long id,
+        @Parameter(description="Data of the review to update.", required=true)
+        @RequestBody @Valid ReviewDto reviewDto) {
+        return super.update(id, reviewDto);
+    }
+
+    @Operation
+    @CrudOperation(operation=DELETE, resource=REVIEW)
+    @DeleteMapping("/{id}")
+    public @NonNull ResponseEntity<Void> delete(
+        @Parameter(description="Unique identifier of the review to delete.", required=true)
+        @PathVariable Long id) {
+        return super.delete(id);
+    }
+/*
+
+   probleme mit dem implementieren bzgl. des approved-status
+
+    @Operation
+    @CrudOperation(operation=FILTER, resource=REVIEW)
+    @GetMapping("/filter")
+    public ResponseEntity<List<ReviewDto>> filter(
+        @RequestParam("pos_id") Long posId ,
+        @RequestParam("approved") Boolean approved
+    ) {
+        return ResponseEntity.ok(
+            reviewDtoMapper.fromDomain(reviewService.getById(posId))//,
+            //reviewDtoMapper.fromDomain(reviewService.getByStatus(approved))
+        );
+    }
+
+    @Operation
+    @CrudOperation(operation=FILTER, resource=REVIEW)
+    @GetMapping("/filter")
+    public ResponseEntity<ReviewDto> approve(
+        @PathVariable Long id ,
+        @RequestParam ("user_id") Long userId
+    ) {
+        //return super.update(id, reviewDto);
+    }
+*/
 }
--- api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
+++ api/src/main/java/de/seuhd/campuscoffee/api/dtos/ReviewDto.java
@@ -2,14 +2,34 @@ package de.seuhd.campuscoffee.api.dtos;
 
 import lombok.Builder;
 import org.jspecify.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDateTime;
 
 /**
  * DTO record for POS metadata.
  */
 @Builder(toBuilder = true)
 public record ReviewDto (
-    @Nullable Long id
-    // TODO: Implement ReviewDto
+        // TODO: x Implement ReviewDto
+    @Nullable Long id,
+    @Nullable LocalDateTime createdAt,
+    @Nullable LocalDateTime updatedAt,
+
+    @NotNull
+    @NonNull Long posId,
+
+    @NotNull
+    @NonNull Long authorId,
+
+    @NotBlank(message = "Review cannot be empty.")
+    @NonNull String review,
+
+    // ich bin überfragt
+    Boolean approved
+
+
 ) implements Dto<Long> {
     @Override
     public @Nullable Long getId() {
